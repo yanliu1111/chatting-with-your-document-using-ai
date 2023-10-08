abbe503e444a46d3b1386b2289d0ae609526f0c6
yanliu1111 <liuyan0504@gmail.com>
2023-10-07 23:03:13 -0600

    feat: install clsx animate lucide-realt ...

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
-import clsx, { ClassValue } from 'clsx';
-
-import { twMerge } from 'tailwind-merge';
-
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
 export function cn(...inputs: ClassValue[]) {
-  //px-2 py-2 -> p-2
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
