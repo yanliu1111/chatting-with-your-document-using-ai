90ca7b4f5187815bb7952c524c58db9954a87617
yanliu1111 <liuyan0504@gmail.com>
2023-10-08 16:38:19 -0600

    feat: trpc setup done

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,10 +1,12 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 
 //this page is for sync the login user, and make sure they are also in db
-const Page = () => {
+const Page = async () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin'); // origin = dashboard
+
+  const apiResponse = await fetch('/api/auth-callback', {});
 };
 
 export default Page;
