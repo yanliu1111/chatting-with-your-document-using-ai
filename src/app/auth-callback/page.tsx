70363f6ce5c2e1f3c865d96e3eb8105e72b8596e
yanliu1111 <liuyan0504@gmail.com>
2023-10-08 16:52:35 -0600

    feat: trpc testing data type from api

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,12 +1,15 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 
+import { trpc } from '../_trpc/client';
+
 //this page is for sync the login user, and make sure they are also in db
 const Page = async () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin'); // origin = dashboard
 
-  const apiResponse = await fetch('/api/auth-callback', {});
+  const { data, isLoading } = trpc.test.useQuery();
 };
 
 export default Page;
+//depend on trpc/index.ts return data type, check auth-callback/page.tsx would return the data in same data type
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,13 @@
-import { router } from './trpc';
+import { publicProcedure, router } from './trpc';
 
 export const appRouter = router({
-  // ...
+  test: publicProcedure.query(() => {
+    return 3;
+  }),
 });
 // Export type router type signature,
 // NOT the router itself.
 export type AppRouter = typeof appRouter;
+
+//publicProcedure create query or mutation, queries are mainly for get requests so purely getting data, and mutations are for post patch and delete requests, for modifying data
+//depend on return data type, check auth-callback/page.tsx would return the data in same data type
