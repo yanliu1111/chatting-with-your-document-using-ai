b125c98322ae482d33b88cf002fcef228222fe48
yanliu1111 <liuyan0504@gmail.com>
2023-10-09 12:15:56 -0600

    feat: dashboard-uploadPDF button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 //use it in server components, not client
 
+import Dashboard from '@/components/Dashboard';
 import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Dialog, DialogTrigger } from './ui/dialog';
+
+import { Button } from './ui/button';
+import { useState } from 'react';
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v);
+        }
+      }}
+    >
+      <DialogTrigger asChild>
+        {/* custom button, not a button that dialog is inherently, aschild makes the Button instead not pre-wrapped in one more button */}
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+    </Dialog>
+  );
+};
+
+export default UploadButton;
