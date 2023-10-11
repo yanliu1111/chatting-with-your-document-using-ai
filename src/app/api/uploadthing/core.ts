22719e2faa16a21d124418d91bf1e3b6b19d2273
yanliu1111 <liuyan0504@gmail.com>
2023-10-11 09:09:34 -0600

    feat: middleware setting for auth

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,7 @@
 /*
 https://docs.uploadthing.com/nextjs/appdir
 */
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 
 const f = createUploadthing();
@@ -8,7 +9,11 @@ const f = createUploadthing();
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: '4MB' } })
     .middleware(async ({ req }) => {
-      return {};
+      //only the authenticated user can upload images
+      const { getUser } = getKindeServerSession();
+      const user = getUser();
+      if (!user || !user.id) throw new Error('Unauthorized');
+      return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {}),
 } satisfies FileRouter;
