b506a79719c94011b2eff548607d5f13fd9bcc80
yanliu1111 <liuyan0504@gmail.com>
2023-10-11 09:00:56 -0600

    feat: setup storage route

---
+++ src/app/api/uploadthing/route.ts
@@ -0,0 +1,9 @@
+//after past from docs, dont need touch again
+
+import { createNextRouteHandler } from 'uploadthing/next';
+import { ourFileRouter } from './core';
+
+// Export routes for Next App Router
+export const { GET, POST } = createNextRouteHandler({
+  router: ourFileRouter,
+});
