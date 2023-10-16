23649f14fdbc9aa8ecf96c86502f66141340cbb8
yanliu1111 <liuyan0504@gmail.com>
2023-10-16 00:19:48 -0600

    feat: 3 loading cases done isload/fail/process

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { ChevronLeft, Loader2, XCircle } from 'lucide-react';
+
 import ChatInput from './ChatInput';
-import { Loader2 } from 'lucide-react';
+import Link from 'next/link';
 import Messages from './Messages';
+import { buttonVariants } from '../ui/button';
 import { trpc } from '@/app/_trpc/client';
 
 interface ChatWrapperProps {
@@ -36,12 +39,51 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
       </div>
     );
 
+  if (data?.status === 'PROCESSING')
+    return (
+      <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+        <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+          <div className='flex flex-col items-center gap-2'>
+            <Loader2 className='h-8 w-8 text-blue-500 animate-spin' />
+            <h3 className='font-semibold text-xl'> PROCESSING PDF...</h3>
+            <p className='text-zinc-500 text-sm'>This won&apos;t take long.</p>
+          </div>
+        </div>
+        <ChatInput isDisabled />
+      </div>
+    );
+
+  if (data?.status === 'FAILED')
+    return (
+      <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+        <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+          <div className='flex flex-col items-center gap-2'>
+            <XCircle className='h-8 w-8 text-red-500' />
+            <h3 className='font-semibold text-xl'> Too many pages in PDF...</h3>
+            <p className='text-zinc-500 text-sm'>
+              Your <span className='font-medium'>Free</span> plan supports up to
+              5 pages.
+            </p>
+            <Link
+              href='/dashboard'
+              className={buttonVariants({
+                variant: 'secondary',
+                className: 'mt-4',
+              })}
+            >
+              <ChevronLeft className='h-3 w-3 mr-1.5' />
+              Back
+            </Link>
+          </div>
+        </div>
+        <ChatInput isDisabled />
+      </div>
+    );
   return (
     <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
       <div className='flex-1 justify-between flex flex-col mb-28'>
         <Messages />
       </div>
-
       <ChatInput />
     </div>
   );
